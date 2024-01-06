65e70b58e519485e7102683fd41c5f15f8817450
MaXeKin <maximilian@sunday.gg>
2024-01-06 19:34:50 +0100

    Trajectory Update

--- Assets/Scripts/TrajectoryScript.cs
+++ Assets/Scripts/TrajectoryScript.cs
@@ -7,6 +7,7 @@ public class TrajectoryScript : MonoBehaviour
 {
    private LineRenderer _lineRenderer;
     private Transform _previousHit;
+    private Transform _previousHit2;
 
     private void Start()
     {
@@ -34,8 +35,15 @@ public class TrajectoryScript : MonoBehaviour
             Vector3 reflectedContactPoint = contactPoint + reflectedDirection * 5f;
 
             // CalculateTrajectory(contactPoint, reflectedContactPoint);
-            Raycast2(contactPoint, reflectedContactPoint);
-            SetLinePoints(contactPoint, reflectedContactPoint);
+            //Raycast2(contactPoint, reflectedContactPoint);
+            
+            //if second raycast doesnt hit another object
+            if (!Raycast2(contactPoint, reflectedDirection))
+            {
+                SetLinePoints(reflectedContactPoint, 2);
+            }
+            
+            SetLinePoints(contactPoint, 1);
 
             // Activate outline
             hit.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
@@ -60,24 +68,49 @@ public class TrajectoryScript : MonoBehaviour
         }
     }
 
-    void Raycast2(Vector3 pos, Vector3 dir)
+    bool Raycast2(Vector3 pos, Vector3 dir)
     {
+        Debug.DrawRay(pos, dir, Color.blue, 1f);
         RaycastHit hit;
-        if (Physics.Raycast(pos, dir, out hit, 1 << 8))
+        if (Physics.Raycast(pos, dir, out hit, Mathf.Infinity, 1<<8))
         {
+            
             // Get exact point of contact
             var contactPoint = hit.point;
-
+            Debug.Log(hit);
             // Activate outline
             hit.transform.GetChild(0).GetComponent<MeshRenderer>().enabled = true;
+            
+            if (_previousHit2 != null && _previousHit2 != hit.transform)
+            {
+                // Disable sprite renderer of the previous hit object
+                _previousHit2.GetChild(0).GetComponent<MeshRenderer>().enabled = false;
+            }
+
+            // Set the current hit object as the previous hit
+            _previousHit2 = hit.transform;
+            //set line
+            SetLinePoints(contactPoint, 2);
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 
     // Set Line Renderer points along raycast.
-    private void SetLinePoints(Vector3 contactPoint, Vector3 secondPoint)
+    private void SetLinePoints(Vector3 contactPoint, int index)
+    {
+        _lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z));
+        _lineRenderer.SetPosition(index, contactPoint);
+        
+    }
+    
+    /*private void SetLinePoints(Vector3 contactPoint, Vector3 secondPoint)
     {
         _lineRenderer.SetPosition(0, new Vector3(transform.position.x, transform.position.y - 0.1f, transform.position.z));
         _lineRenderer.SetPosition(1, contactPoint);
         _lineRenderer.SetPosition(2, secondPoint);
-    }
+    }*/
 }
