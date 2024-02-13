69910d0b639b58d01000efae74d18c58fb1cb1fb
Maximilian Kinney <maxekinney@yahoo.com>
2024-02-13 14:18:35 +0100

    right follows ball

--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,47 +6,62 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     //follow ball variables
+    public Transform player;
     private Transform _activeBullet;
-    private int _activeBulletIndex;
+    public int _activeBulletIndex;
 
 
     private void Awake()
     {
-        throw new NotImplementedException();
+        
     }
 
     private IEnumerator FollowBall()
     {
         int prevIndex = _activeBulletIndex;
-        _activeBullet = GameManager.Instance.activeBullets[_activeBulletIndex].transform;
-        //transform.parent = _activeBullet;
-        Debug.Log(_activeBullet);
+        _activeBullet = GameManager.Instance.activeBullets[_activeBulletIndex-1].transform;
+        transform.parent = null;
+        Debug.Log(_activeBulletIndex);
         while (_activeBulletIndex == prevIndex && _activeBullet != null)
         {
-            //take into account velocity of ball for camera angle
-            Debug.Log("active");
+            var velocity = _activeBullet.GetComponent<Rigidbody>().velocity.normalized;
+
+            // Calculate the target position
+            Vector3 targetPosition = _activeBullet.position - velocity;
+
+            // Gradually move the camera towards the target position
+            // transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+            transform.position = targetPosition;
+            // Look at the bullet
+            transform.LookAt(_activeBullet);
+            
             yield return null;
         }
-        Debug.Log("endOfCouroutine");
+
+       
+        Debug.Log(prevIndex);
 
         //transform.parent = GameManager.Instance.player.transform;
     }
-    
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            Debug.Log(_activeBulletIndex+1);
+            Debug.Log(_activeBulletIndex);
             Debug.Log(GameManager.Instance.activeBullets.Count);
             if (_activeBulletIndex < GameManager.Instance.activeBullets.Count)
             {
-                Debug.Log("right arrow");
-                StartCoroutine(FollowBall());
                 _activeBulletIndex++;
+                StartCoroutine(FollowBall());
+                
             }
             else
             {
                 _activeBulletIndex = 0;
+                transform.parent = player;
+                transform.localPosition = Vector3.zero;
+                transform.localEulerAngles = Vector3.zero;
             }
 
             
