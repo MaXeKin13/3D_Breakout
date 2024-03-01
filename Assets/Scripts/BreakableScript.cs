e363919ef9489a66580329ebbf702319f37ac6bf
Maximilian Kinney <maxekinney@yahoo.com>
2024-03-01 11:28:12 +0100

    Disco Balls change Color when Hit

--- Assets/Scripts/BreakableScript.cs
+++ Assets/Scripts/BreakableScript.cs
@@ -10,17 +10,23 @@ public class BreakableScript : MonoBehaviour
     public bool isTransparent;
     [Space]
     public int health;
-    private MeshRenderer _mat;
+    
     [HideInInspector] public MeshRenderer highlight;
     [HideInInspector] public Vector3 xNormal;
+
+    private MeshRenderer _mat;
+    public SpriteAnim _spriteAnim;
     private void Start()
     {
         xNormal = transform.right;
         _mat = GetComponent<MeshRenderer>();
-        Debug.Log(_mat.material);
+
+        _spriteAnim = GetComponentInChildren<SpriteAnim>();
         //ChangeColor();
         highlight = transform.GetChild(0).GetComponent<MeshRenderer>();
         ChangeMaterial();
+
+        ChangeDisco();
         
     }
 
@@ -34,6 +40,7 @@ public class BreakableScript : MonoBehaviour
     {
         health--;
         ChangeMaterial();
+        ChangeDisco();
         ScoreManager.Instance.AddScore(1);
         if(health <= 0)
             Death();
@@ -65,6 +72,27 @@ public class BreakableScript : MonoBehaviour
                     break;
                 case 1:
                     _mat.material = VisualManager.Instance.blockMaterials[0];
+                    break;
+            }
+    }
+
+    void ChangeDisco()
+    {
+       
+            switch (health)
+            {
+                case 4:
+                    //Get List of DiscoUI class, then get the array of sprites in discoUI[4]
+                    _spriteAnim.sprites = VisualManager.Instance.discoUI[3].discos;
+                    break;
+                case 3:
+                    _spriteAnim.sprites = VisualManager.Instance.discoUI[2].discos;
+                    break;
+                case 2:
+                    _spriteAnim.sprites = VisualManager.Instance.discoUI[1].discos;
+                    break;
+                case 1:
+                    _spriteAnim.sprites = VisualManager.Instance.discoUI[0].discos;
                     break;
             }
     }
--- Assets/Scripts/Managers/VisualManager.cs
+++ Assets/Scripts/Managers/VisualManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,13 +11,18 @@ public class VisualManager: MonoBehaviour
     public GameObject trajectory;
     public GameObject cursor;
     [Space]
+    
     public Material[] blockMaterials;
     public GameObject blockDestroySystem;
 
     [Space]
     public GameObject GameplayUI;
     public GameObject WinUI;
-    
+
+
+   
+    [SerializeField] public List<DiscoUI> discoUI = new List<DiscoUI>();
+
     void Awake()
     {
         Instance = (Instance == null) ? this : Instance;
@@ -26,5 +32,11 @@ public class VisualManager: MonoBehaviour
     {
         GameplayUI.SetActive(false);
         WinUI.SetActive(true);
+    }
+
+    [Serializable] 
+    public struct DiscoUI
+    {
+        public Sprite[] discos;        
     }
 }
